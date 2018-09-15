c19e97bfc439a53d11d515e18490f4746a463414
tangxiaodong <tangxiaodong@jd.com>
2018-09-15 19:54:08 +0800

    commit

---
+++ src/test/java/com/uuola/webapp/Test.java
@@ -0,0 +1,32 @@
+/*
+ * @(#)Test.java 2018年9月15日
+ * 
+ * Copy Right@ uuola
+ */ 
+
+package com.uuola.webapp;
+
+import com.uuola.webapp.constants.DATE_FORMAT;
+import com.uuola.webapp.util.DateUtil;
+import com.uuola.webapp.util.IdGenerator;
+
+/**
+ * <pre>
+ *
+ * @author tonydon
+ * 创建日期: 2018年9月15日
+ * </pre>
+ */
+public class Test {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println(DateUtil.parseDate("2018-08-01 00:00:00", DATE_FORMAT.YYYY_MM_DD_HH_MM_SS).getTime());
+        for(int k=0; k<100; k++)
+        System.out.println(IdGenerator.INSTANCE.nextId());
+    }
+
+}
