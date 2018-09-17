a2532cd94360f3cd03063bb22177e397ece5f707
tangxiaodong <tangxiaodong@jd.com>
2018-09-17 21:06:29 +0800

    commit

--- src/test/java/com/uuola/webapp/Test.java
+++ src/test/java/com/uuola/webapp/Test.java
@@ -21,9 +21,9 @@ public class Test {
 
     /**
      * @param args
+     * @throws InterruptedException 
      */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(DateUtil.parseDate("2018-08-01 00:00:00", DATE_FORMAT.YYYY_MM_DD_HH_MM_SS).getTime());
         for(int k=0; k<100; k++)
         System.out.println(IdGenerator.INSTANCE.nextId());
