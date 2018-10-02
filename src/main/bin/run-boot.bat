f536759ceae9cafe135befd4841dd3b0ff20229a
tangxiaodong <tangxiaodong@jd.com>
2018-10-02 16:35:20 +0800

    COMMIT

--- src/main/bin/run-boot.bat
+++ src/main/bin/run-boot.bat
@@ -1,2 +1,6 @@
-java -jar -server -Xms512m -Xmx512m -Xss512k ./lib/bootapp-1.0.jar --spring.config.location=file:./config/ --spring.resources.static-locations=file:./static/ -Dfile.dir=./static/files/
-@pause
+set "CURR_DIR=%~dp0%"
+set "MAIN_CLASS=org.springframework.boot.loader.JarLauncher"
+set "SPRING_CFG_PATH=--spring.config.location=file:%CURR_DIR%\config\ --spring.resources.static-locations=file:%CURR_DIR%\static\"
+set "JAVA_OPTS=-server -Xms512m -Xmx512m -Xss512k -Dfiles.dir=%CURR_DIR%\static\files\"
+set "CLASSPATH=%CURR_DIR%;%CURR_DIR%\lib;%CURR_DIR%\lib\bootapp-1.0.jar"
+java %JAVA_OPTS% -cp %CLASSPATH% %MAIN_CLASS% %SPRING_CFG_PATH% 
